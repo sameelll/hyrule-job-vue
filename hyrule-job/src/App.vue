b84d5205577c754ca01b09faf11cdbc24f975b54
Eren Akel <eren.akel.46@gmail.com>
2021-11-12 14:09:18 +0300

    the project has been initialized

---
+++ hyrule-job/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app">
+    <p> {{ jobs[0].id  }}  </p>
+  </div>  
+</template>
+
+<script lang="ts">
+import { defineComponent, ref,} from 'vue';
+import Job from './types/job'
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  setup(){
+    const jobs = ref<Job[]>([
+      {
+        title: 'farm-worker' ,
+        location: 'lon lon ranch' ,
+        salary: 30000,
+        id: '1'
+      },
+      { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+      { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+      { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+      { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+    ])
+
+    return { jobs }
+  }
+});
+</script>
+
+<style>
+
+</style>
