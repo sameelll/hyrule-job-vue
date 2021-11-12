7da655866f962932ca350b781e8c727fd1c8d585
Eren Akel <eren.akel.46@gmail.com>
2021-11-12 14:57:17 +0300

    buttons have been initiated

--- hyrule-job/src/App.vue
+++ hyrule-job/src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="app">
-    <JobList :jobs="jobs" />
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')" >order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    
+    <JobList :jobs="jobs" :order="order" />
   </div>  
 </template>
 
@@ -8,6 +16,7 @@
 import { defineComponent, ref} from 'vue';
 import JobList from './components/JobList.vue'
 import Job from './types/job';
+import OrderTerm from './types/OrderTerm'
 
 export default defineComponent({
   name: 'App',
@@ -26,11 +35,32 @@ export default defineComponent({
       { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
     ])
 
-    return { jobs }
+    const order = ref<OrderTerm>('title')
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order }
   }
 });
 </script>
 
-<style>
-
+<style scoped>
+  header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
 </style>
